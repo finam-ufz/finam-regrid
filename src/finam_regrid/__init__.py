eb5d8e53ef5b2252136c9a72c8e489c627de8f99
Martin Lange <martin.lange@ufz.de>
2022-11-11 12:40:10 +0100

    make installable without setup.cfg

--- src/finam_regrid/__init__.py
+++ src/finam_regrid/__init__.py
@@ -1,1 +1,7 @@
 """ESMF/ESMPy regridding components for FINAM"""
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:  # pragma: no cover
+    # package is not installed
+    __version__ = "0.0.0.dev0"
