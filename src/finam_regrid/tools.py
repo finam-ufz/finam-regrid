fc466f549ad5c5ee794130b9a669df15662c4c0c
Sebastian Müller <mueller.seb@posteo.de>
2024-03-07 17:16:44 +0100

    tools: add support for 3D meshes

--- src/finam_regrid/tools.py
+++ src/finam_regrid/tools.py
@@ -4,18 +4,24 @@ from __future__ import annotations
 import esmpy
 import finam as fm
 import numpy as np
+from finam.data.grid_tools import ESMF_TYPE_MAP
 from pyproj import Transformer, crs
-
-ESMF_CELLTYPES = {
-    fm.CellType.TRI: esmpy.api.constants.MeshElemType.TRI,
-    fm.CellType.QUAD: esmpy.api.constants.MeshElemType.QUAD,
-}
 
 ESMF_DIM_NAMES = ["ESMF:X", "ESMF:Y", "ESMF:Z"]
 
-ESMF_STAGGER_LOC = {
+ESMF_STAGGER_LOC_2D = {
     fm.Location.CELLS: esmpy.StaggerLoc.CENTER,
     fm.Location.POINTS: esmpy.StaggerLoc.CORNER,
+}
+
+ESMF_STAGGER_LOC_3D = {
+    fm.Location.CELLS: esmpy.StaggerLoc.CENTER_VCENTER,
+    fm.Location.POINTS: esmpy.StaggerLoc.CORNER_VCENTER,
+}
+
+ESMF_STAGGER_LOC = {
+    2: ESMF_STAGGER_LOC_2D,
+    3: ESMF_STAGGER_LOC_3D,
 }
 
 ESMF_MESH_LOC = {
@@ -62,25 +68,30 @@ def to_esmf(grid, transformer=None):
 
 def _to_esmf_grid(grid: fm.data.StructuredGrid, transformer):
     dims = np.array([d - 1 for d in grid.dims], dtype=np.int32)
-    loc = ESMF_STAGGER_LOC[grid.data_location]
+    grid_dim = grid.mesh_dim
+    loc = ESMF_STAGGER_LOC[grid_dim][grid.data_location]
 
     g = esmpy.Grid(
         dims,
         staggerloc=[],
         coord_sys=esmpy.CoordSys.CART,
     )
-    g.add_coords(staggerloc=esmpy.StaggerLoc.CENTER)
-    g.add_coords(staggerloc=esmpy.StaggerLoc.CORNER)
+    g.add_coords(staggerloc=ESMF_STAGGER_LOC[grid_dim][fm.Location.POINTS])
+    g.add_coords(staggerloc=ESMF_STAGGER_LOC[grid_dim][fm.Location.CELLS])
 
     points = _transform_points(transformer, grid.points)
-    cells = _transform_points(transformer, grid.cell_centers)
+    cell_centers = _transform_points(transformer, grid.cell_centers)
 
-    for i in range(len(grid.axes)):
-        grid_corner = g.get_coords(i, staggerloc=esmpy.StaggerLoc.CORNER)
-        grid_center = g.get_coords(i, staggerloc=esmpy.StaggerLoc.CENTER)
+    for i in range(grid.dim):
+        grid_corner = g.get_coords(
+            i, staggerloc=ESMF_STAGGER_LOC[grid_dim][fm.Location.POINTS]
+        )
+        grid_center = g.get_coords(
+            i, staggerloc=ESMF_STAGGER_LOC[grid_dim][fm.Location.CELLS]
+        )
 
         grid_corner[...] = points[:, i].reshape(grid.dims, order=grid.order)
-        grid_center[...] = cells[:, i].reshape(dims, order=grid.order)
+        grid_center[...] = cell_centers[:, i].reshape(dims, order=grid.order)
 
     field = esmpy.Field(g, name=grid.name, staggerloc=loc)
     field.data[:] = np.nan
@@ -97,26 +108,29 @@ def _to_esmf_mesh(grid: fm.UnstructuredGrid, transformer):
         coord_sys=esmpy.CoordSys.CART,
     )
 
-    node_ids = np.arange(grid.point_count)
+    num_node = grid.point_count
     points = _transform_points(transformer, grid.points)
 
     # Does for some reason create weird coordinates with `parametric_dim=2, spatial_dim=3`
-    # Therefore removing the z coordinate
-    node_coords = np.array([p[:2] for p in points]).flatten()
-    node_owner = np.zeros(grid.point_count)
+    mesh.add_nodes(
+        node_count=num_node,
+        node_ids=np.arange(num_node) + 1,
+        node_coords=points.reshape(-1),
+        node_owners=np.zeros(num_node),
+    )
 
-    mesh.add_nodes(grid.point_count, node_ids, node_coords, node_owner)
-
-    elems = grid.cells
-    num_elems = elems.shape[0]
-    elem_ids = np.arange(num_elems)
-    elem_types = np.asarray([ESMF_CELLTYPES[e] for e in grid.cell_types])
+    num_elem = grid.cell_count
+    elem_types = ESMF_TYPE_MAP[grid.cell_types]
+    if np.any(elem_types == -1):
+        # this should only occure for line elements in 1D
+        # vertices are covered by the UnstructuredPoints class
+        raise ValueError(f"ESMF can't be used to regrid 1D data.")
 
     mesh.add_elements(
-        num_elems,
-        elem_ids,
-        elem_types,
-        elems.flatten(),
+        element_count=num_elem,
+        element_ids=np.arange(num_elem) + 1,
+        element_types=elem_types,
+        element_conn=grid.cells_connectivity,
         element_coords=grid.cell_centers,
     )
 
